35aa9df0195f1115d7ae6a00e5679c62fa1779d5
MikolajMys <mikolaj.my@gmail.com>
2024-05-19 17:26:05 +0200

    singleton design pattern

--- singleton/Main.java
+++ singleton/Main.java
@@ -1,8 +1,52 @@
 package singleton;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+class ToDoManager {
+    private static ToDoManager instance;
+    private List<String> tasks;
+
+    private ToDoManager() {
+        tasks = new ArrayList<String>();
+    }
+
+    public static ToDoManager getInstance() {
+        if(instance == null){
+            instance = new ToDoManager();
+        }
+        return instance;
+    }
+
+    public void addTask(String task){
+        tasks.add(task);
+        System.out.println("'" + task + "'" + " added to list");
+    }
+
+    public void removeTask(String task) {
+        if (tasks.remove(task)) {
+            System.out.println("'" + task + "' removed from list");
+        } else {
+            System.out.println("'" + task + "' not found in the list");
+        }
+    }
+
+    public void showTasks(){
+        System.out.println("Remain tasks:\n");
+        for(String task : tasks){
+            System.out.println("- "+task);
+        }
+    }
+}
 class Client {
     void performAction() {
-        System.out.println("hello");
+        ToDoManager toDoManager = ToDoManager.getInstance();
+        toDoManager.addTask("Buy groceries");
+        toDoManager.addTask("Clean the house");
+        toDoManager.addTask("Pay bills");
+        toDoManager.showTasks();
+        toDoManager.removeTask("Clean the house");
+        toDoManager.showTasks();
     }
 }
 
